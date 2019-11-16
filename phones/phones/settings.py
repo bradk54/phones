7fb23ad02a68a83e9ab36c73c4070aa73773f0dc
Brad Kittrell <bradkittrell@Brads-MacBook-Pro.local>
2019-11-16 14:38:29 -0500

    new

--- phones/phones/settings.py
+++ phones/phones/settings.py
@@ -67,9 +67,7 @@ DOWNLOAD_DELAY = 5
 
 # Configure item pipelines
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'phones.pipelines.PhonesPipeline': 300,
-#}
+ITEM_PIPELINES = {'phones.pipelines.PhonesPipeline': 300}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/autothrottle.html
--- phones/phones/spiders/phonecrawl.py
+++ phones/phones/spiders/phonecrawl.py
@@ -18,25 +18,9 @@ class PhonecrawlSpider(scrapy.Spider):
             url = response.urljoin(href.extract())
             yield scrapy.Request(url,callback = self.parse_dat)
 
-        #
-        # links = response.xpath('//div[@class="makers"]/ul/li/a/@href').extract()
-        # for link in links:
-        #     link_dict = {'url':url+link}
-        #     yield link_dict
-    # def parse(self, response):
-    #     links = response.xpath('//div[@class="makers"]/ul/li/a/@href').extract()
-    #     for i in links:
-    #        yield Request(urlparse.urljoin('', i[1:]),callback=self.parse_dat)
-    #
-    #
-    #
+
     def parse_dat(self,response):
         for sel in response.xpath('//h1[contains(@class,"specs-phone-name-title")]'):
             item = PhonesItem()
             item['title'] = sel.xpath('text()').extract()
             yield item
-
-
-        # title = response.xpath('//h1[contains(@class,"specs-phone-name-title")]/text()').extract_first().strip()
-        # phone_list = {'phone':title}
-        # yield phone_list
