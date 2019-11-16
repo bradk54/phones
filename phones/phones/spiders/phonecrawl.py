576a6cbd9df4bf060225990b2a0ee3dadb003119
Brad Kittrell <bradkittrell@brads-mbp.lan>
2019-11-16 13:09:21 -0500

    new changes

--- phones/phones/spiders/phonecrawl.py
+++ phones/phones/spiders/phonecrawl.py
@@ -4,19 +4,25 @@ from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider,Rule,Spider
 import string
 from scrapy import Request
+from phones.items import PhonesItem
 # import pandas as pd
 url = 'https://www.gsmarena.com/'
 class PhonecrawlSpider(scrapy.Spider):
     name = 'phonecrawl'
     allowed_domains = ['www.gsmarena.com']
-    start_urls = ['http://www.gsmarena.com/results.php3?']
+    start_urls = ["http://www.gsmarena.com/results.php3?",]
     rules = [Rule(LinkExtractor(allow=('/pn\d+')),callback='parse',follow=True)]
 
     def parse(self, response):
-        links = response.xpath('//div[@class="makers"]/ul/li/a/@href').extract()
-        for link in links:
-            link_dict = {'url':url+link}
-            yield link_dict
+        for href in response.xpath('//div[@class="makers"]/ul/li/a/@href'):
+            url = response.urljoin(href.extract())
+            yield scrapy.Request(url,callback = self.parse_dat)
+
+        #
+        # links = response.xpath('//div[@class="makers"]/ul/li/a/@href').extract()
+        # for link in links:
+        #     link_dict = {'url':url+link}
+        #     yield link_dict
     # def parse(self, response):
     #     links = response.xpath('//div[@class="makers"]/ul/li/a/@href').extract()
     #     for i in links:
@@ -24,7 +30,13 @@ class PhonecrawlSpider(scrapy.Spider):
     #
     #
     #
-    # def parse_dat(self,response):
-    #     title = response.xpath('//h1[contains(@class,"specs-phone-name-title")]/text()').extract_first().strip()
-    #     phone_list = {'phone':title}
-    #     yield phone_list
+    def parse_dat(self,response):
+        for sel in response.xpath('//h1[contains(@class,"specs-phone-name-title")]'):
+            item = PhonesItem()
+            item['title'] = sel.xpath('text()').extract()
+            yield item
+
+
+        # title = response.xpath('//h1[contains(@class,"specs-phone-name-title")]/text()').extract_first().strip()
+        # phone_list = {'phone':title}
+        # yield phone_list
